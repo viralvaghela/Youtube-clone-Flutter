d683aeb241a6bbbc13d91509a4cf996f10d7aa47
viralvaghela <viralvaghela7109@gmail.com>
2020-10-19 15:40:07 +0530

    json updated

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:youtube/widgets/videoCard.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -9,18 +12,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var url =
+      "https://raw.githubusercontent.com/viralvaghela/Youtube-clone-Flutter/master/videos.json";
+
+  bool isLoading = true;
+  List data;
+
+  @override
+  void initState() {
+    super.initState();
+    getVideos();
+  }
+
+  void getVideos() async {
+    var response = await http.get(url);
+    setState(() {
+      data = jsonDecode(response.body);
+      isLoading = false;
+    });
+    print(data.length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          VideoCard(),
-          VideoCard(),
-          VideoCard(),
-          VideoCard(),
-          VideoCard(),
-        ],
-      ),
-    );
+        child: isLoading == true
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                children: data.map((item) {
+                  return VideoCard(
+                    thumbnail: item['thumbnail'],
+                    title: item['title'],
+                    channelProfilePicUrl: item['channelProfilePicUrl'],
+                    channelName: item['channelName'],
+                    viewCount: item['viewCount'],
+                    uploadTime: item['uploadTime'],
+                    videoUrl: item['videoUrl'],
+                  );
+                }).toList(),
+              ));
   }
 }
--- lib/widgets/videoCard.dart
+++ lib/widgets/videoCard.dart
@@ -3,6 +3,25 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class VideoCard extends StatelessWidget {
+  final String thumbnail;
+  final String title;
+  final String channelProfilePicUrl;
+  final String channelName;
+  final String viewCount;
+  final String uploadTime;
+  final String videoUrl;
+
+  const VideoCard(
+      {Key key,
+      this.thumbnail,
+      this.title,
+      this.channelProfilePicUrl,
+      this.channelName,
+      this.viewCount,
+      this.uploadTime,
+      this.videoUrl})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Card(
